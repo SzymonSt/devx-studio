bd644b1ef75fccfaca70c83a55be1813ed46a440
SzymonSt <szymonst2808@gmail.com>
2023-05-12 22:27:49 +0200

    cf frontend cd

--- devx-studio/src/interafces/continuousfeedback.ts
+++ devx-studio/src/interafces/continuousfeedback.ts
@@ -23,7 +23,7 @@ export interface EventSurvey{}
 export interface IntegrationSurvey{}
 
 export interface AudienceItem{
-    
+    id: string;
 }
 export interface Question{
     questionId: string;
